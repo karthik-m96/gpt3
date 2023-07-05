17eda062fa5ab097a0178fc038f9f84fc33c0e4f
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-05 12:50:35 +0530

    changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,8 +26,8 @@ const Navbar = () => {
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu
-          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+          ? <RiCloseLine color="white" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="white" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
