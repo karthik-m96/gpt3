ba9f77a7c1bfb1c7b2f3f19d41955767ea985c50
Karthik M <karthikm300796@gmail.com>
2023-07-10 00:23:16 +0530

    blog & article

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <div className="gpt3__cta section__padding">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h2>Register today & start exploring the endless possiblities.</h2>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
